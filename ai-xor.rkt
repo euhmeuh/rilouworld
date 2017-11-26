c17739d8cffc0780a0885f04bb29005f56ed9645
euhmeuh <merdelin.mortetombe@laposte.net>
2017-11-26 17:28:13 +0100

    experiment with neural networks for AI

---
+++ ai-xor.rkt
@@ -0,0 +1,50 @@
+#lang racket
+
+;;; An Artificial Neural Network that learns the rules of binary XOR.
+
+(require "ai.rkt")
+
+(define layer1 (make-object layer% 4 3))
+(displayln "layer 1: 4 neurons with 3 inputs:")
+(display-matrix (get-field weights layer1))
+(define layer2 (make-object layer% 1 4))
+(displayln "layer 2: 1 neuron with 4 inputs:")
+(display-matrix (get-field weights layer2))
+(define network (list layer1 layer2))
+
+;; sample inputs and results for XOR
+;; the third column is meaningless, we expect the network to discover that
+(define inputs (make-float-matrix '[[0.0 0.0 1.0]
+                                    [0.0 1.0 1.0]
+                                    [1.0 0.0 1.0]
+                                    [0.0 1.0 0.0]
+                                    [1.0 0.0 0.0]
+                                    [1.0 1.0 1.0]
+                                    [0.0 0.0 0.0]]))
+(define results (make-float-matrix '[[0.0] [1.0] [1.0] [1.0] [1.0] [0.0] [0.0]]))
+
+;; training
+(displayln "training...")
+(train-all inputs results 60000 network)
+
+(displayln "layer1 after training:")
+(display-matrix (get-field weights layer1))
+(displayln "layer 2 after training:")
+(display-matrix (get-field weights layer2))
+
+(displayln "results with same inputs as training:")
+;; should be similar to the expected results
+(display-matrix (propagate inputs network))
+
+(displayln "results with new inputs:")
+;; try new inputs not seen before
+(display-matrix (propagate (make-float-matrix '[[1.0 1.0 0.0]
+                                                [1.0 0.0 1.0]
+                                                [1.0 1.0 1.0]
+                                                [1.0 0.0 1.0]
+                                                [0.0 1.0 1.0]
+                                                [0.0 0.0 0.0]
+                                                [0.0 1.0 1.0]
+                                                [1.0 0.0 1.0]
+                                                [1.0 1.0 1.0]])
+                            network))
---
+++ ai.rkt
@@ -0,0 +1,101 @@
+#lang racket
+;; make racket think we're running a typed racket
+;; so that matrix operations run inside typed/racket
+;; but not the rest of our program
+(require live-free-or-die)
+(live-free-or-die!)
+
+(require math)
+(provide
+  layer%
+  make-float-matrix
+  display-matrix
+  propagate
+  train-all)
+
+(define T matrix-transpose)
+
+(define (make-float-matrix 2d-list)
+  (list*->matrix 2d-list))
+
+(define (sigmoid x)
+  (/ (+ 1.0 (exp (- x)))))
+
+(define (sigmoid-deriv x)
+  (let ([y (sigmoid x)])
+    (* y (- 1.0 y))))
+
+(define (matrix-sig m)
+  (matrix-map sigmoid m))
+
+(define (matrix-dsig m)
+  (matrix-map sigmoid-deriv m))
+
+(define (matrix-mul m n)
+  (matrix-map * m n))
+
+(define (make-random-matrix rows cols)
+  (build-matrix rows cols (lambda (x y) (random))))
+
+(define (make-weights rows cols)
+  (matrix-map sub1
+    (matrix-scale (make-random-matrix rows cols) 2)))
+
+(define layer%
+  (class object%
+    (init nb-of-neurons inputs-per-neuron)
+
+    (field [weights (make-weights inputs-per-neuron nb-of-neurons)]
+           [outputs (make-float-matrix '[[0.0]])]) ;; we need to remember outputs to calculate error delta later
+
+    (super-new)
+
+    (define/public (process inputs)
+      (set! outputs (matrix-sig (matrix* inputs weights)))
+      outputs)
+
+    (define/public (update inputs delta)
+      (set! weights (matrix+ weights
+                             (matrix* (T inputs) delta)))
+      weights)))
+
+(define (propagate inputs layers)
+  (if (pair? layers)
+    (propagate (send (car layers) process inputs) (cdr layers))
+    inputs))
+
+(define (backpropagate first-inputs expected-outputs layers)
+  (define (get-upper-inputs layers)
+    (if (and (pair? layers) (not (empty? (cdr layers))))
+      (get-field outputs (cadr layers))
+      first-inputs))
+
+  (define (get-delta outputs errors)
+    (matrix-mul errors (matrix-dsig outputs)))
+
+  (define (train layers previous-errors)
+    (unless (empty? layers)
+      (let* ([layer (car layers)]
+             [delta (get-delta (get-field outputs layer) previous-errors)]
+             [errors (matrix* delta (T (get-field weights layer)))]
+             [inputs (get-upper-inputs layers)])
+        (send layer update inputs delta)
+        (train (cdr layers) errors))))
+
+  (define first-errors (matrix- expected-outputs (get-field outputs (last layers))))
+
+  (train (reverse layers) first-errors))
+
+(define (train-all inputs expected-outputs iterations layers)
+  (for ((_ iterations))
+    (propagate inputs layers)
+    (backpropagate inputs expected-outputs layers)))
+
+(define (display-results matrix)
+  (displayln (matrix->list matrix)))
+
+(define (display-matrix matrix)
+  (for ((i (in-range (matrix-num-rows matrix))))
+    (for ((j (in-range (matrix-num-cols matrix))))
+      (printf "~a\t" (matrix-ref matrix i j)))
+    (newline)))
