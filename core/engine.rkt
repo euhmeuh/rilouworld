baaa0ba5d38483b97213c8e77611c5b7cb87eb16
euhmeuh <merdelin.mortetombe@laposte.net>
2018-01-03 23:40:21 +0100

    handle animations differently when building the sprite-db

--- core/engine.rkt
+++ core/engine.rkt
@@ -19,6 +19,7 @@
   (prefix-in gl: mode-lambda/backend/gl)
   "../utils/log.rkt"
   "../utils/struct.rkt"
+  "../utils/anaphora.rkt"
   (prefix-in quest: "../quest/entities.rkt"))
 
 (define *nb-of-layers* 8)
@@ -41,22 +42,52 @@
 
 (struct engine-options (width height title))
 
+(define (sprite-ref name index)
+  (string->symbol (format "~a/~a" name index)))
+
 (define (make-database resources)
   (define sprite-db (make-sprite-db))
   (add-palette!/file sprite-db 'palette (build-path "." "dimensions" "palette.png"))
   (for-each
     (lambda (resource)
-      (with-values resource (name path) from quest:resource
-        (set! path (build-path "." "dimensions" path))
-        (if (file-exists? path)
-          (add-sprite!/file sprite-db
-                            name
-                            path
-                            #:palette 'palette)
-          (warning 'sprite-build
-                   "Could not find file '~a' for resource '~a'." path name))))
+      (cond
+        [(quest:animation? resource) (handle-animation resource sprite-db)]
+        [else (handle-resource resource sprite-db)]))
     resources)
   (compile-sprite-db sprite-db))
+
+(define (handle-resource resource sprite-db)
+  (with-values resource (name path) from quest:resource
+    (set! path (build-path "." "dimensions" path))
+    (if (file-exists? path)
+        (add-sprite!/file sprite-db
+                          name
+                          path
+                          #:palette 'palette)
+        (warning 'sprite-build
+                 "Could not find file '~a' for resource '~a'." path name))))
+
+(define (handle-animation resource sprite-db)
+  (with-values resource (name path) from quest:resource
+    (with-values resource (size frames) from quest:animation
+      (set! path (build-path "." "dimensions" path))
+      (aif (open-bitmap path)
+           (for ([bitmap (cut-image-in-parts it size frames)]
+                 [index (in-naturals 0)])
+             (add-sprite!/bm sprite-db
+                             (sprite-ref name index)
+                             (thunk bitmap)
+                             #:palette 'palette))
+           (warning 'sprite-build
+                    "Could not find file '~a' for resource '~a'." path name)))))
+
+(define (open-bitmap path)
+  (local-require racket/draw)
+  (and (file-exists? path)
+       (read-bitmap path)))
+
+(define (cut-image-in-parts bitmap size frames)
+  (list bitmap))
 
 (define (make-render-context width height sprite-db)
   (gl:stage-draw/dc sprite-db width height *nb-of-layers*))
@@ -82,12 +113,18 @@
 (define (render-sprite the-sprite sprite-db)
   (with-values the-sprite (pos resource layer) from quest:sprite
     (sprite (quest:pos-x pos) (quest:pos-y pos)
-            (sprite-idx sprite-db (quest:resource-name resource))
+            (sprite-idx sprite-db (find-current-sprite resource))
             #:layer layer
             #:pal-idx (palette-idx sprite-db 'palette))))
 
 (define (render-static-sprite sprite sprite-db)
   #f)
+
+(define (find-current-sprite resource)
+  (cond
+    [(quest:animation? resource)
+     (sprite-ref (quest:resource-name resource) 0)]
+    [else (quest:resource-name resource)]))
 
 (define (make-loop options dimension account)
   (define sprite-db
