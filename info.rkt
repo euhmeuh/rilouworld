34009df1540cf42a36f1112cfd511dc5db05dcc1
Jérôme Martin <euhmeuh@users.noreply.github.com>
2019-02-28 16:20:36 +0100

    add missing deps

--- info.rkt
+++ info.rkt
@@ -1,9 +1,23 @@
 #lang info
 
-(define deps '("base" "anaphoric" "web-galaxy" "mode-lambda" "reprovide-lang"))
-(define build-deps '())
 (define pkg-desc "The decentralized game universe")
 (define pkg-authors '(euhmeuh))
 (define version "0.1.0")
 
 (define scribblings '(("scribblings/rilouworld.scrbl" (multi-page))))
+
+(define deps
+  (list
+    "base"
+    "math-lib"
+    "draw-lib"
+    "anaphoric"
+    "web-server-lib" ;; TODO: replace with "web-galaxy"
+    "mode-lambda"
+    "lux"
+    "reprovide-lang"))
+
+(define build-deps
+  (list
+    "racket-doc"
+    "scribble-lib"))
