d641ae782ae97773e08aafcdfab72b1f9772a15b
euhmeuh <jerome.martin.dev@gmail.com>
2019-03-26 12:53:11 +0100

    refactor with-values into a syntax-parse
    + remove anaphora module

--- private/utils/struct.rkt
+++ private/utils/struct.rkt
@@ -7,34 +7,33 @@
   with-values)
 
 (require
-  (for-syntax racket/base))
+  (for-syntax
+    racket/base
+    racket/syntax
+    syntax/parse))
 
-(define-for-syntax (make-id stx template . ids)
-  (define str (apply format template (map syntax->datum ids)))
-  (datum->syntax stx (string->symbol str)))
+(define-for-syntax (make-getters stx instance struct-name fields)
+  (datum->syntax stx
+    (map (lambda (field)
+           #`(#,field (#,(format-id stx "~a-~a" struct-name field) #,instance)))
+         (syntax->list fields))))
 
-(define-for-syntax (make-getters stx object struct-name values)
-  (for/list ([value (syntax->list values)])
-    `(,value (,(make-id stx "~a-~a" struct-name value) ,object))))
-
-;; Example:
-;;
-;; (struct ingredient (name taste price color))
-;; (define carrot (ingredient "Carrot" 'good 2.0 'orange))
-;;
-;; (with-values carrot (name taste) from ingredient
-;;   (displayln (format "This is a ~a that tastes ~a!" name taste))
-;;
 (define-syntax (with-values stx)
-  (syntax-case stx (from)
-    [(_ s (value ...) from struct body ...)
-     (with-syntax ([getters (make-getters stx #'s #'struct #'(value ...))])
-       #'(let getters
-           body ...))]))
+  (syntax-parse stx
+    #:datum-literals (from)
+    [(_ <instance> (<field> ...) from <struct> <body> ...)
+     #:with (getters ...) (make-getters stx #'<instance> #'<struct> #'(<field> ...))
+     #'(let (getters ...)
+         <body> ...)]))
 
-#| test
-(struct character (name size))
-(define s (character "Merdelin" 12))
-(with-values s (name size) from character
-  (displayln (format "I am ~a and my size is ~a!" name size)))
-|#
+(module+ test
+  (require rackunit)
+
+  (struct character (name size))
+  (define my-char (character "Merdelin" 12))
+  (check-equal?
+    (with-values my-char (name size) from character
+      (format "I am ~a and my size is ~a!" name size))
+    "I am Merdelin and my size is 12!")
+
+)
